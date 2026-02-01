e86c638e421dd9cd6906d37b6c3b1d754927db5a
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-02-01 15:06:07 +0530

    changed something

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=Suresh
+person2=Ramesh
+echo "$person1: Hi, $person2"
+echo "$person2: Hi, $person1, How are you?"
+echo "$person1: Hi $person2, I am good what about you?"
+echo "$person2: I am also fine thanks for asking"
