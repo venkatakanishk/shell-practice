3ba7326c6b321e86c7cd2f7433c8ff3feaad40eb
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-02-01 18:59:06 +0530

    changed something

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 num=$1
-if [ $num -gt 20 ]; then
-    echo "given number: $num is greater than 20"
+if [ $num -gt 25 ]; then
+    echo "given number: $num is greater than 25"
 elif [ $num -eq 25 ]; then
       echo "given number: $num is equal to 25"
 else
