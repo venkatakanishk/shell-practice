8b6d3515e45639d585d2325dc2f6fa09b19a9d9e
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-02-01 15:30:21 +0530

    changed something

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-FILE_NAME=$(touch) aws
+FILE_NAME="aws.txt"
+touch "$FILE_NAME"
 TIME=$(date)
 echo "script executed at : $TIME "
 echo "file name is created with name : $FILE_NAME at time : $TIME"
