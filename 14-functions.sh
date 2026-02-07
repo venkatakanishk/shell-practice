beb872b94cf9fba79793af0dd59125d3521c4633
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-02-07 22:36:59 +0530

    changed something

--- 14-functions.sh
+++ 14-functions.sh
@@ -6,6 +6,7 @@ if [ $USERID -ne 0 ]; then
     echo "please run th4e script with root user access"
     exit 1
 fi
+
 echo "installing nginx"
 dnf install nginx -y
 
@@ -13,4 +14,12 @@ if [ $? -ne 0 ]; then
     echo "Nginx installation is failure"
 else
     echo "Nginx installation is success"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Mysql installation is failure"
+else
+    echob "Mysql installation is success"
 fi
