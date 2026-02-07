d0f7f6951b3c891bef23bc16842fd5c9aeca61c3
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-02-07 23:45:43 +0530

    changed something

---
+++ 16-tee-command-log.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "please run th4e script with root user access" | tee -a $LOG_FILE
+    exit 1
+fi
+mkdir -p $LOG_FOLDER
+VALIDATE (){
+    if [ $1 -ne 0 ]; then
+        echo "$2 is failure" | tee -a $LOG_FILE
+        exit 1
+    else 
+        echo "$2 is success" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install nodejs -y &>> $LOG_FILE
+VALIDATE $? "installing nodejs"
+
+dnf install mysql &>> $LOG_FILE
+VALIDATE $? "installing mysql"
+
+dnf install java -y &>> $LOG_FILE
+VALIDATE $? "installing java"
