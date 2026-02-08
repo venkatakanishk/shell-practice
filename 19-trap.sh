1284daad9e7b307fb6f5437ff83bec564ae60a52
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-02-08 14:41:57 +0530

    changed something

--- 19-trap.sh
+++ 19-trap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e #ERR
-trap 'echo "There is an error in the $LINENO, COMMAND: $BASH_COMMAND"' ERR
+trap 'echo "There is an error in the Line: $LINENO, COMMAND: $BASH_COMMAND"' ERR
 echo "Kanishk"
 echoo "Age:26"
 echo "Male"
